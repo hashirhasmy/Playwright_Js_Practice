71c88faa27efde3a8bb4dfabd9de01f9798cd845
hashirhasmy <hashirhasmy111@gmail.com>
2025-12-22 15:33:33 +0530

    playwright config enhancement

--- playwright.config.js
+++ playwright.config.js
@@ -19,9 +19,9 @@ export default defineConfig({
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
-  retries: process.env.CI ? 2 : 0,
+  retries: process.env.CI ? 2 : 1,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
+  workers: process.env.CI ? 4 : 1,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
@@ -30,7 +30,12 @@ export default defineConfig({
     // baseURL: 'http://localhost:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
+    trace: "off",
+    video: "off",
+    screenshot: "off",
+    headless: false,
+    ignoreHTTPSErrors: true,
+    actionTimeout: 20000,
   },
 
   /* Configure projects for major browsers */
@@ -77,5 +82,9 @@ export default defineConfig({
   //   url: 'http://localhost:3000',
   //   reuseExistingServer: !process.env.CI,
   // },
+  timeout: 600 * 1000,
+  expect: {
+    timeout: 20000,
+  },
 });
 
